06d0820f3fbe68410edaa828196f4e8e6b93dee2
Stanislav Novitski <stani2980@hotmail.com>
2018-10-05 10:10:40 +0200

    added lock

--- simulator.py
+++ simulator.py
@@ -8,26 +8,26 @@ import requests
 import time
 import sys
 import logging
-from multiprocessing import Pool, Process
+from multiprocessing import Pool, Process, Lock
 
 
 # time_to_wait = 2
         
-def make_request(count):
-    print(count)
+def make_request(l, count):
     logging.basicConfig(filename='info.log', level=logging.INFO)
     epoch_time_before = time.time()
     response = requests.get(sys.argv[1]) # url to ping
     if response.status_code == 200:
         epoch_time_after = time.time()
+        l.acquire()
         logging.log(logging.INFO, "It took " + str(epoch_time_after - epoch_time_before) + " seconds to perform this request. Process: " + str(count))
         print(f'Process: {count}')
-
+        l.release()
 
 if __name__ == '__main__':
-    for i in range(1, int(sys.argv[2])): # amount of requests
-        ##with Pool(4) as pool:
-        ##  pool.map(make_request, [])
+        lock = Lock()
+        
+        for i in range(int(sys.argv[2])):
+            Process(target=make_request, args=(lock,i)).start()
+        
 
-        p = Process(target=make_request(1), args=([i]))
-        p.start()
